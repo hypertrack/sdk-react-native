12feedd614cbef0f6220a36ff4162a5e8e6ac82a
sathyapriya31 <sathyapriya2006@gmail.com>
2021-03-19 19:31:24 +0530

    views SDK added

--- android/src/main/java/com/hypertrack/reactnative/androidsdk/HTSDKModule.java
+++ android/src/main/java/com/hypertrack/reactnative/androidsdk/HTSDKModule.java
@@ -1,7 +1,5 @@
 package com.hypertrack.reactnative.androidsdk;
-
 import android.util.Log;
-
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -14,29 +12,33 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.hypertrack.sdk.HyperTrack;
 import com.hypertrack.sdk.TrackingError;
 import com.hypertrack.sdk.TrackingStateObserver;
-
 import java.util.Locale;
-
+import com.hypertrack.sdk.views.HyperTrackViews;
+import androidx.core.util.Consumer;
+import com.google.gson.Gson;
+import com.hypertrack.sdk.views.DeviceUpdatesHandler;
+import com.hypertrack.sdk.views.dao.Location;
+import com.hypertrack.sdk.views.dao.MovementStatus;
+import com.hypertrack.sdk.views.dao.StatusUpdate;
+import com.hypertrack.sdk.views.dao.Trip;
+import androidx.annotation.NonNull;
 
 @ReactModule(name = HTSDKModule.NAME)
 public class HTSDKModule extends ReactContextBaseJavaModule {
-
     private static final String TAG = "HTSDKModule";
-
     public static final String NAME = "HyperTrack";
-
     public TrackingStateObserver.OnTrackingStateChangeListener trackingStateChangeListener;
     public HyperTrack sdkInstance;
-
+    public DeviceUpdatesHandler deviceUpdatesHandler;
+    public HyperTrackViews mHyperTrackView;
+     final Gson gson = new Gson();
     public HTSDKModule(ReactApplicationContext reactContext) {
         super(reactContext);
     }
-
     @Override
     public String getName() {
         return NAME;
     }
-
     @ReactMethod
     public void initialize(String publishableKey, Boolean startsTracking, Boolean automaticallyRequestPermissions, Promise promise) {
         try {
@@ -46,6 +48,7 @@ public class HTSDKModule extends ReactContextBaseJavaModule {
             if (startsTracking) {
                 sdkInstance.start();
             }
+            mHyperTrackView= HyperTrackViews.getInstance(getReactApplicationContext().getApplicationContext() , publishableKey);
             promise.resolve(true);
         } catch (java.lang.Exception exception) {
             Log.e(TAG, "Hypertrack SDK initialization failed.",  exception);
@@ -53,7 +56,6 @@ public class HTSDKModule extends ReactContextBaseJavaModule {
             promise.reject(error, new Exception("Hypertrack SDK initialization failed."));
         }
     }
-
     @ReactMethod
     public void subscribeOnEvents() {
         if (trackingStateChangeListener != null) {
@@ -70,14 +72,12 @@ public class HTSDKModule extends ReactContextBaseJavaModule {
                         .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
                         .emit("onTrackingErrorHyperTrack", params);
             }
-
             @Override
             public void onTrackingStart() {
                 getReactApplicationContext()
                         .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
                         .emit("onTrackingStartHyperTrack", null);
             }
-
             @Override
             public void onTrackingStop() {
                 getReactApplicationContext()
@@ -87,33 +87,26 @@ public class HTSDKModule extends ReactContextBaseJavaModule {
         };
         sdkInstance.addTrackingListener(trackingStateChangeListener);
     }
-
     @ReactMethod
     public void getDeviceID(Promise promise) {
         promise.resolve(sdkInstance.getDeviceID());
     }
-
     @ReactMethod
     public void isTracking(Promise promise) {
         promise.resolve(sdkInstance.isRunning());
     }
-
-
     @ReactMethod
     public void startTracking() {
         sdkInstance.start();
     }
-
     @ReactMethod
     public void stopTracking() {
         sdkInstance.stop();
     }
-
     @ReactMethod
     public void syncDeviceSettings() {
         sdkInstance.syncDeviceSettings();
     }
-
     @ReactMethod
     public void setTripMarker(ReadableMap rMap, Promise promise) {
         try {
@@ -124,7 +117,6 @@ public class HTSDKModule extends ReactContextBaseJavaModule {
             promise.reject(error, e);
         }
     }
-
     @ReactMethod
     public void setDeviceName(String name, Promise promise) {
         try {
@@ -135,7 +127,6 @@ public class HTSDKModule extends ReactContextBaseJavaModule {
             promise.reject(error, e);
         }
     }
-
     @ReactMethod
     public void setMetadata(ReadableMap rMap, Promise promise) {
         try {
@@ -146,26 +137,79 @@ public class HTSDKModule extends ReactContextBaseJavaModule {
             promise.reject(error, e);
         }
     }
-
     @ReactMethod
     public void enableDebugLogging(Boolean enable) {
         if (enable) {
             HyperTrack.enableDebugLogging();
         }
     }
-
     @ReactMethod
     public void enableMockLocation(Boolean enable) {
         if (enable) {
             sdkInstance.allowMockLocations();
         }
     }
-
+   @ReactMethod
+    public void getDeviceMovementStatus(final Promise promise){
+    mHyperTrackView.getDeviceMovementStatus(sdkInstance.getDeviceID(),
+                new Consumer<MovementStatus>() {
+                    @Override
+                    public void accept(MovementStatus movementStatus) { 
+                        promise.resolve(gson.toJson(movementStatus));
+                    }
+                });
+    }
+    @ReactMethod
+    public void subscribeToDeviceUpdates(final Promise promise){
+        if (deviceUpdatesHandler != null) {
+            mHyperTrackView.stopAllUpdates();
+            deviceUpdatesHandler = null;
+        }
+    mHyperTrackView.subscribeToDeviceUpdates(sdkInstance.getDeviceID(),
+                deviceUpdatesHandler=new DeviceUpdatesHandler() {
+                    @Override
+                    public void onLocationUpdateReceived(@NonNull Location location) {
+                       getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onLocationUpdateReceived", gson.toJson(location));
+                    }
+                    @Override
+                    public void onBatteryStateUpdateReceived(@MovementStatus.BatteryState int i) {
+                       getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onBatteryStateUpdateReceived", i);
+                    }
+                    @Override
+                    public void onStatusUpdateReceived(@NonNull StatusUpdate statusUpdate) {
+                         getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onStatusUpdateReceived", gson.toJson(statusUpdate));
+                    }
+                    @Override
+                    public void onTripUpdateReceived(@NonNull Trip trip) {
+                       getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onTripUpdateReceived", gson.toJson(trip));
+                    }
+                    @Override
+                    public void onError(Exception exception, String deviceId) {
+                        getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onErrorDeviceUpdates", exception);
+                    }
+                    @Override
+                    public void onCompleted(String deviceId) {
+                         getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("onCompletedDeviceUpdates", deviceId);
+                    }
+                });
+}
+   
     private static final int RN_ERROR_GENERAL = 0;
     private static final int RN_ERROR_INVALID_PUBLISHABLE_KEY = 1;
     private static final int RN_ERROR_AUTHORIZATION_FAILED = 2;
     private static final int RN_ERROR_PERMISSION_DENIED = 3;
-
     private int convertToRNErrorsCode(int androidSdkCode) {
         switch (androidSdkCode) {
             case TrackingError.INVALID_PUBLISHABLE_KEY_ERROR:
--- js/HyperTrack.js
+++ js/HyperTrack.js
@@ -1,116 +1,190 @@
-'use strict';
-
-import EventEmitter from './EventEmitter';
-import {Error} from './CriticalErrors';
-
-const HyperTrack = require('react-native').NativeModules.HyperTrack;
-
+"use strict";
+import EventEmitter from "./EventEmitter";
+import { Error } from "./CriticalErrors";
+const HyperTrack = require("react-native").NativeModules.HyperTrack;
 class HyperTrackAPI {
-
-    /**
-     * Returns a string which is used by HyperTrack to uniquely identify the user.
-     */
-    getDeviceID(): Promise<string>  {
-        return HyperTrack.getDeviceID();
-    }
-
-    /**
-     * Determine whether the SDK is tracking the movement of the user.
-     * @return {boolean} Whether the user's movement data is getting tracked or not.
-     */
-    isTracking(): Promise<boolean> {
-        return HyperTrack.isTracking();
-    }
-
-    /**
-     * Update device state from server.
-     */
-    syncDeviceSettings() {
-        return HyperTrack.syncDeviceSettings();
-    }
-
-    /**
-     * Start or resume tracking location and activity events.
-     */
-    startTracking() {
-        HyperTrack.startTracking();
-    }
-
-    /**
-     * Stops the SDK from listening to the user's movement updates and recording any data.
-     */
-    stopTracking() {
-        HyperTrack.stopTracking();
-    }
-
-    /**
-     * Registers tracking state callbacks.
-     * @param {Object} component - Instance of your component.
-     * @param {function} onError - Tracking state callback for error events.
-     * @param {function} onStart - Tracking state callback for start event.
-     * @param {function} onStop - Tracking state callback for stop event.
-     */
-    registerTrackingListeners(component: Object,
-                         onError: (error: ?Error) => void,
-                         onStart: () => void,
-                         onStop: () => void) {
-        if (onError) component.onErrorHyperTrackSubscription = EventEmitter.addListener('onTrackingErrorHyperTrack', onError);
-        if (onStart) component.onStartHyperTrackSubscription = EventEmitter.addListener('onTrackingStartHyperTrack', onStart);
-        if (onStop) component.onStopHyperTrackSubscription = EventEmitter.addListener('onTrackingStopHyperTrack', onStop);
-        HyperTrack.subscribeOnEvents();
-    }
-
-    /**
-     * Removes tracking state callbacks.
-     * @param {Object} component - Instance of your component.
-     */
-    unregisterTrackingListeners(component: Object) {
-        component.onErrorHyperTrackSubscription && component.onErrorHyperTrackSubscription.remove();
-        component.onStartHyperTrackSubscription && component.onStartHyperTrackSubscription.remove();
-        component.onStopHyperTrackSubscription && component.onStopHyperTrackSubscription.remove();
-    }
-
-    /**
-     * Send device name to HyperTrack
-     * @param {string} name - Device name you want to see in the Dashboard.
-     */
-    setDeviceName(name: string) {
-        HyperTrack.setDeviceName(name);
-    }
-
-    /**
-     * Send metadata details to HyperTrack
-     * @param {Object} data - Send extra device information.
-     */
-    setMetadata(data: Object) {
-        HyperTrack.setMetadata(data);
-    }
-
-    /**
-     * Set a trip marker
-     * @param {Object} data - Include anything that can be parsed into JSON.
-     */
-    setTripMarker(data: Object) {
-        HyperTrack.setTripMarker(data);
-    }
-
+  /**
+   * Returns a string which is used by HyperTrack to uniquely identify the user.
+   */
+  getDeviceID(): Promise<string> {
+    return HyperTrack.getDeviceID();
+  }
+  getDeviceMovementStatus(): Promise<Object> {
+    return HyperTrack.getDeviceMovementStatus();
+  }
+  /**
+   * Determine whether the SDK is tracking the movement of the user.
+   * @return {boolean} Whether the user's movement data is getting tracked or not.
+   */
+  isTracking(): Promise<boolean> {
+    return HyperTrack.isTracking();
+  }
+  /**
+   * Update device state from server.
+   */
+  syncDeviceSettings() {
+    return HyperTrack.syncDeviceSettings();
+  }
+  /**
+   * Start or resume tracking location and activity events.
+   */
+  startTracking() {
+    HyperTrack.startTracking();
+  }
+  /**
+   * Stops the SDK from listening to the user's movement updates and recording any data.
+   */
+  stopTracking() {
+    HyperTrack.stopTracking();
+  }
+  /**
+   * Registers tracking state callbacks.
+   * @param {Object} component - Instance of your component.
+   * @param {function} onError - Tracking state callback for error events.
+   * @param {function} onStart - Tracking state callback for start event.
+   * @param {function} onStop - Tracking state callback for stop event.
+   */
+  registerTrackingListeners(
+    component: Object,
+    onError: (error: ?Error) => void,
+    onStart: () => void,
+    onStop: () => void
+  ) {
+    if (onError)
+      component.onErrorHyperTrackSubscription = EventEmitter.addListener(
+        "onTrackingErrorHyperTrack",
+        onError
+      );
+    if (onStart)
+      component.onStartHyperTrackSubscription = EventEmitter.addListener(
+        "onTrackingStartHyperTrack",
+        onStart
+      );
+    if (onStop)
+      component.onStopHyperTrackSubscription = EventEmitter.addListener(
+        "onTrackingStopHyperTrack",
+        onStop
+      );
+    HyperTrack.subscribeOnEvents();
+  }
+  /**
+   * Removes tracking state callbacks.
+   * @param {Object} component - Instance of your component.
+   */
+  unregisterTrackingListeners(component: Object) {
+    component.onErrorHyperTrackSubscription &&
+      component.onErrorHyperTrackSubscription.remove();
+    component.onStartHyperTrackSubscription &&
+      component.onStartHyperTrackSubscription.remove();
+    component.onStopHyperTrackSubscription &&
+      component.onStopHyperTrackSubscription.remove();
+  }
+  /**
+   * Send device name to HyperTrack
+   * @param {string} name - Device name you want to see in the Dashboard.
+   */
+  setDeviceName(name: string) {
+    HyperTrack.setDeviceName(name);
+  }
+  /**
+   * Send metadata details to HyperTrack
+   * @param {Object} data - Send extra device information.
+   */
+  setMetadata(data: Object) {
+    HyperTrack.setMetadata(data);
+  }
+  /**
+   * Set a trip marker
+   * @param {Object} data - Include anything that can be parsed into JSON.
+   */
+  setTripMarker(data: Object) {
+    HyperTrack.setTripMarker(data);
+  }
+  registerDeviceUpdatesListeners(
+    component: Object,
+    onError: (error: ?Error) => void,
+    onLocationUpdate: (location: ?Object) => void,
+    onBatteryStateUpdate: (batteryState: ?Int) => void,
+    onStatusUpdate: (statusUpdate: ?Object) => void,
+    onTripUpdate: (tripUpdate: ?Object) => void,
+    onCompletedDeviceUpdate: (onComplete: ?string) => void
+  ) {
+    if (onError)
+      component.onErrorDeviceUpdatesSubscription = EventEmitter.addListener(
+        "onErrorDeviceUpdates",
+        onError
+      );
+    if (onLocationUpdate)
+      component.onLocationUpdateSubscription = EventEmitter.addListener(
+        "onLocationUpdateReceived",
+        onLocationUpdate
+      );
+    if (onBatteryStateUpdate)
+      component.onBatteryStateUpdateSubscription = EventEmitter.addListener(
+        "onBatteryStateUpdateReceived",
+        onBatteryStateUpdate
+      );
+    if (onStatusUpdate)
+      component.onStatusUpdateSubscription = EventEmitter.addListener(
+        "onStatusUpdateReceived",
+        onStatusUpdate
+      );
+    if (onTripUpdate)
+      component.onTripUpdateSubscription = EventEmitter.addListener(
+        "onTripUpdateReceived",
+        onTripUpdate
+      );
+    if (onCompletedDeviceUpdate)
+      component.onCompletedDeviceUpdateSubscription = EventEmitter.addListener(
+        "onCompletedDeviceUpdates",
+        onCompletedDeviceUpdate
+      );
+    HyperTrack.subscribeToDeviceUpdates();
+  }
+  /**
+   * Removes tracking state callbacks.
+   * @param {Object} component - Instance of your component.
+   */
+  unregisterTrackingListeners(component: Object) {
+    component.onErrorDeviceUpdatesSubscription &&
+      component.onErrorDeviceUpdatesSubscription.remove();
+    component.onLocationUpdateSubscription &&
+      component.onLocationUpdateSubscription.remove();
+    component.onBatteryStateUpdateSubscription &&
+      component.onBatteryStateUpdateSubscription.remove();
+    component.onStatusUpdateSubscription &&
+      component.onStatusUpdateSubscription.remove();
+    component.onTripUpdateSubscription &&
+      component.onTripUpdateSubscription.remove();
+    component.onCompletedDeviceUpdateSubscription &&
+      component.onCompletedDeviceUpdateSubscription.remove();
+  }
 }
-
 module.exports = {
-    /**
-     * Initialize the HyperTrack SDK
-     * @param {string} publishableKey - A unique String that is used to identify your account with HyperTrack.
-     * @param {boolean} automaticallyRequestPermissions - If true, SDK automatically triggers location and motion activity permissions dialogs when tracking starts.
-     */
-    async createInstance(publishableKey, automaticallyRequestPermissions = true): Promise<HyperTrackAPI> {
-        await HyperTrack.initialize(publishableKey, false, automaticallyRequestPermissions);
-        return new HyperTrackAPI();
-    },
-    /**
-     * Enables debug log output. This is very verbose, so you shouldn't use this for
-     * production build.
-     */
-    enableDebugLogging(enable) {
-        HyperTrack.enableDebugLogging(enable);
-    },
+  /**
+   * Initialize the HyperTrack SDK
+   * @param {string} publishableKey - A unique String that is used to identify your account with HyperTrack.
+   * @param {boolean} automaticallyRequestPermissions - If true, SDK automatically triggers location and motion activity permissions dialogs when tracking starts.
+   */
+  async createInstance(
+    publishableKey,
+    automaticallyRequestPermissions = true
+  ): Promise<HyperTrackAPI> {
+    await HyperTrack.initialize(
+      publishableKey,
+      false,
+      automaticallyRequestPermissions
+    );
+    return new HyperTrackAPI();
+  },
+  /**
+   * Enables debug log output. This is very verbose, so you shouldn't use this for
+   * production build.
+   */
+  enableDebugLogging(enable) {
+    HyperTrack.enableDebugLogging(enable);
+  },
+  enableMockLocation(enable) {
+    HyperTrack.enableMockLocation(enable);
+  },
 };
