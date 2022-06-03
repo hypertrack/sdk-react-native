7c07b336ef246ded04d06bb02808c5f580a98682
Egor Timoshenko <poter.t1mox@gmail.com>
2022-06-03 09:09:36 +0300

    init react-native-libriary

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  HyperTrackSdkExample
+//
+
+import Foundation
---
+++ ios/HyperTrackSdk.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface HyperTrackSdk : NSObject <RCTBridgeModule>
+
+@end
