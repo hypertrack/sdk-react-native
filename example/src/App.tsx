0a38a8cdbf67e4334c33d5a2525b3f742a57803d
Egor Timoshenko <poter.t1mox@gmail.com>
2022-06-09 12:42:32 +0300

    added needed permission in Info.plist for example project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, StatusBar } from 'react-native';
 import HyperTrack from 'react-native-hypertrack-sdk';
 
 export default function App() {
@@ -14,36 +14,18 @@ export default function App() {
   };
   React.useEffect(() => {
     getID();
-    const a = HyperTrack.onIncrementIncreasedChanged((data) => {
-      console.log('onIncrement received', data);
-    });
-    const b = HyperTrack.onIncrementDecreasedChanged((data) => {
-      console.log('onDecrement received', data);
-    });
+    // const a = HyperTrack.onIncrementIncreasedChanged((data) => {
+    //   console.log('onIncrement received', data);
+    // });
+    // const b = HyperTrack.onIncrementDecreasedChanged((data) => {
+    //   console.log('onDecrement received', data);
+    // });
 
-    return () => {
-      a.remove();
-      b.remove();
-    };
+    // return () => {
+    //   a.remove();
+    //   b.remove();
+    // };
   }, []);
-
-  const increase = async () => {
-    try {
-      const response = await HyperTrack.increase();
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const decrease = async () => {
-    try {
-      const response = await HyperTrack.decrease();
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   const startTracking = async () => {
     try {
@@ -109,17 +91,18 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      <Button title="Increase" onPress={increase} />
-      <Button title="Decrease" onPress={decrease} />
-      <Button title="Start" onPress={startTracking} />
-      <Button title="Stop" onPress={stopTracking} />
-      <Button title="getlocation" onPress={getLocation} />
-      <Button title="isTracking" onPress={isTracking} />
-      <Button title="isRunning" onPress={isRunning} />
-      <Button title="availability" onPress={availability} />
-      <Button title="device name" onPress={setDeviceName} />
-    </View>
+    <>
+      <StatusBar barStyle={'dark-content'} />
+      <View style={styles.container}>
+        <Button title="Start" onPress={startTracking} />
+        <Button title="Stop" onPress={stopTracking} />
+        <Button title="getlocation" onPress={getLocation} />
+        <Button title="isTracking" onPress={isTracking} />
+        <Button title="isRunning" onPress={isRunning} />
+        <Button title="availability" onPress={availability} />
+        <Button title="device name" onPress={setDeviceName} />
+      </View>
+    </>
   );
 }
 
