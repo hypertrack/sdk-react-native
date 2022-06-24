c0f38d0d47a5bd9e83d5fc48a97828b2f820d009
Egor Timoshenko <poter.t1mox@gmail.com>
2022-06-24 14:01:11 +0300

    implemented SDK methods on iOS side

--- ios/HyperTrack.swift
+++ ios/HyperTrack.swift
@@ -5,10 +5,9 @@ import HyperTrack
 class HyperTrackSdk: RCTEventEmitter{
 
     private var hasListeners = false
-    private var count = 0
     private var listeners: [String: HyperTrack.Cancel] = [:]
 
-//    let hyperTrack = try! HyperTrack(publishableKey: .init(Bundle.main.object(forInfoDictionaryKey: "HyperTrackPublishableKey") as! String)!)
+    //    let hyperTrack = try! HyperTrack(publishableKey: .init(Bundle.main.object(forInfoDictionaryKey: "HyperTrackPublishableKey") as! String)!)
 
     override init(){
         super.init()
@@ -20,7 +19,7 @@ class HyperTrackSdk: RCTEventEmitter{
     }
 
     override func supportedEvents() -> [String]! {
-        return ["onIncrement", "onDecrement", "onLocationChanged", "onTrackingStateChanged", "onErrors"]
+        return ["onIncrement", "onDecrement", "onLocationChanged", "onTrackingStateChanged", "onErrors", "onAvailableStateChanged"]
     }
 
     @objc
@@ -30,18 +29,15 @@ class HyperTrackSdk: RCTEventEmitter{
 
     override func startObserving() {
         hasListeners = true;
-
         // setup listeners
     }
 
     override func stopObserving() {
         hasListeners = false;
-
         // remove listeners
     }
 
     func addListener(_ name: String, listener: @escaping HyperTrack.Cancel) {
-        if listeners.contains (where: { $0.key == name }) { return }
         listeners[name] = listener
     }
 
@@ -51,24 +47,59 @@ class HyperTrackSdk: RCTEventEmitter{
     }
 
     @objc
-    func increment(_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
-        count += 1;
-        resolve(count)
-
-        sendEvent(withName: "onIncrement", body: ["count increased", count])
+    func getName(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+        resolve(HyperTrack.name)
     }
 
     @objc
-    func decrement(_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
-        if(count == 0) {
-            let error = NSError(domain: "", code: 200, userInfo: nil )
-            reject("ERROR_COUNT", "count cannot be negative", error)
-        } else {
-            count -= 1;
-            resolve("count is \(count)")
+    func setName(_ name: String, resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
+        HyperTrack.name = name
+        resolve("Device name has been set")
+    }
 
-            sendEvent(withName: "onDecrement", body: ["count decreased", count])
+    @objc
+    func getMetadata(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+        resolve(HyperTrack.metadata.json.toBridge())
+    }
+
+    @objc
+    func setMetadata(_ metadata: [String: Any], resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
+        metadata.forEach {
+            let value = HyperTrack.JSON.fromBridge($0.value)
+            HyperTrack.metadata[$0.key] = value
         }
+        resolve("Successfuly set")
+    }
+
+    @objc
+    func getLocation(_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
+        switch HyperTrack.location {
+        case .success(let location):
+            resolve(location.json.toBridge())
+        case .failure(let error):
+            resolve(error.json.toBridge())
+        }
+    }
+
+    @objc
+    func isAvailable(_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
+        resolve(HyperTrack.isAvailable)
+    }
+
+    @objc
+    func setAvailability(_ availability: Bool, resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
+        HyperTrack.isAvailable = availability
+        resolve("Availability has been set to \(availability)")
+    }
+
+    @objc
+    func isTracking(_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
+        resolve(HyperTrack.isTracking)
+    }
+
+    @objc
+    func getErrors(_ resolve:RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+        resolve(HyperTrack.errors.map { $0.json.toBridge() })
     }
 
     @objc
@@ -84,52 +115,36 @@ class HyperTrackSdk: RCTEventEmitter{
     }
 
     @objc
-    func getLocation(_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
-        switch HyperTrack.location {
-            case .success(let location):
-              resolve(location.json.toBridge())
-
-            case .failure(let error):
-            func returnResult(value: String) {
-              resolve(value)
-            }
-            switch error {
-                case .notRunning:
-                    returnResult(value: "notRunning")
-                case .starting:
-                    returnResult(value: "starting")
-                case .errors(let errorsSet):
-                    resolve(errorsSet.map { $0.string })
-                default:
-                    resolve("something went wrong")
-            }
+    func addGeotag(_ metadata: [String: Any], resolve:RCTPromiseResolveBlock, rejecter reject:RCTPromiseRejectBlock) -> Void {
+        var dictionary:[String:HyperTrack.JSON] = [:]
+        metadata.forEach {
+            let value = HyperTrack.JSON.fromBridge($0.value)
+            dictionary[$0.key] = value
+        }
+        let result = HyperTrack.addGeotag(dictionary)
+        switch result {
+        case .success(let location):
+            resolve(location.json.toBridge())
+        case .failure(let error):
+            resolve(error.json.toBridge())
         }
     }
+
     @objc
-    func isTracking(_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
-        resolve(HyperTrack.isTracking)
+    func sync(_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
+        HyperTrack.sync()
+        resolve("Synced")
     }
 
     @objc
-    func availability(_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
-        resolve(HyperTrack.isAvailable)
-    }
-
-    @objc
-    func setDeviceName(_ deviceName: String, resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
-        HyperTrack.name = deviceName
-        resolve("Device name set")
-    }
-
-    @objc
-    func subscribeToLocation(_ deviceName: String, resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
+    func subscribeToLocation(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
         let cancel = HyperTrack.subscribeToLocation(){
             result in
 
             switch result {
-                case .success(let location):
-                self.sendEvent(withName: "onLocationChanged", body: ["latitude": location.latitude, "longitude": location.longitude])
-                case .failure(let error):
+            case .success(let location):
+                self.sendEvent(withName: "onLocationChanged", body: location.json.toBridge())
+            case .failure(let error):
                 print("subscribeToLocation error \(error)")
             }
 
@@ -139,12 +154,21 @@ class HyperTrackSdk: RCTEventEmitter{
     }
 
     @objc
-    func subscribeToIsTracking(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+    func subscribeToAvailability(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+        let cancel = HyperTrack.subscribeToIsAvailable() { isAvailable in
+            self.sendEvent(withName: "onAvailableStateChanged", body: isAvailable)
+        }
+        addListener("unsubscribeToAvailability", listener: cancel)
+        resolve("subscribed")
+    }
+
+    @objc
+    func subscribeToTracking(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
         let handler: (Bool) -> Void = { (result: Bool) in
             self.sendEvent(withName: "onTrackingStateChanged", body: result)
         }
         let cancel = HyperTrack.subscribeToIsTracking(completionHandler: handler)
-        addListener("unsubscribeToIsTracking", listener: cancel)
+        addListener("unsubscribeToTracking", listener: cancel)
         resolve("subscribed")
 
     }
@@ -152,7 +176,7 @@ class HyperTrackSdk: RCTEventEmitter{
     @objc
     func subscribeToErrors(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
         let cancel = HyperTrack.subscribeToErrors() { errorsSet in
-            self.sendEvent(withName: "onErrors", body: errorsSet.map { $0.string })
+            self.sendEvent(withName: "onErrors", body: errorsSet.map { $0.json.toBridge() })
         }
         addListener("unsubscribeToErrors", listener: cancel)
         resolve("subscribed")
@@ -160,14 +184,19 @@ class HyperTrackSdk: RCTEventEmitter{
 
     @objc
     func cancelSubscription(_ subscriptionName: String, resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
-        if listeners.contains (where: { $0.key == subscriptionName }) {
-            let cancel = listeners[subscriptionName]
-            if (cancel != nil) {
-                listeners.removeValue(forKey: subscriptionName)
-                cancel!()
-                resolve("unsubscribed")
-            }
+        if let listener = listeners.removeValue(forKey: subscriptionName) {
+            listener()
+            resolve("unsubscribed")
         }
+    }
+
+    @objc
+    func cancelAllSubscriptions(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+        listeners.forEach {
+            $0.value()
+        }
+        listeners.removeAll()
+        resolve("unsubscribed")
     }
 }
 
@@ -175,62 +204,66 @@ class HyperTrackSdk: RCTEventEmitter{
 
 // source: https://github.com/launchdarkly/react-native-client-sdk/blob/517f8205ba9a610da08fff9e75cd4321bd42623e/ios/LaunchdarklyReactNativeClient.swift#L324-L356
 extension HyperTrack.JSON {
-  static func fromBridge(_ value: Any) -> HyperTrack.JSON {
-    guard !(value is NSNull) else { return .null }
-    if let nsNumValue = value as? NSNumber {
-        // Because we accept `LDValue` in contexts that can receive anything, the value is a
-        // reference type in Objective-C. Because of that, RN bridges the type as a `NSNumber`,
-        // so we must determine whether that `NSNumber` was originally created from a `BOOL`.
-        // Adapted from https://stackoverflow.com/a/30223989
-        let boolTypeId = CFBooleanGetTypeID()
-        if CFGetTypeID(nsNumValue) == boolTypeId {
-            return .bool(nsNumValue.boolValue)
-        } else {
-            return .number(Double(truncating: nsNumValue))
+    static func fromBridge(_ value: Any) -> HyperTrack.JSON {
+        guard !(value is NSNull) else { return .null }
+        if let nsNumValue = value as? NSNumber {
+            // Because we accept `LDValue` in contexts that can receive anything, the value is a
+            // reference type in Objective-C. Because of that, RN bridges the type as a `NSNumber`,
+            // so we must determine whether that `NSNumber` was originally created from a `BOOL`.
+            // Adapted from https://stackoverflow.com/a/30223989
+            let boolTypeId = CFBooleanGetTypeID()
+            if CFGetTypeID(nsNumValue) == boolTypeId {
+                return .bool(nsNumValue.boolValue)
+            } else {
+                return .number(Double(truncating: nsNumValue))
+            }
+        }
+        if let stringValue = value as? String { return .string(stringValue) }
+        if let arrayValue = value as? [Any] { return .array(arrayValue.map { fromBridge($0) }) }
+        if let dictValue = value as? [String: Any] { return .object(dictValue.mapValues { fromBridge($0) }) }
+        return .null
+    }
+
+    func toBridge() -> Any {
+        switch self {
+        case .null: return NSNull()
+        case .bool(let boolValue): return boolValue
+        case .number(let numValue): return numValue
+        case .string(let stringValue): return stringValue
+        case .array(let arrayValue): return arrayValue.map { $0.toBridge() }
+        case .object(let objectValue): return objectValue.mapValues { $0.toBridge() }
+        @unknown default: fatalError("unhandled case")
         }
     }
-    if let stringValue = value as? String { return .string(stringValue) }
-    if let arrayValue = value as? [Any] { return .array(arrayValue.map { fromBridge($0) }) }
-    if let dictValue = value as? [String: Any] { return .object(dictValue.mapValues { fromBridge($0) }) }
-    return .null
-  }
-
-  func toBridge() -> Any {
-    switch self {
-    case .null: return NSNull()
-    case .bool(let boolValue): return boolValue
-    case .number(let numValue): return numValue
-    case .string(let stringValue): return stringValue
-    case .array(let arrayValue): return arrayValue.map { $0.toBridge() }
-    case .object(let objectValue): return objectValue.mapValues { $0.toBridge() }
-    @unknown default: fatalError("unhandled case")
-    }
-  }
 }
 
 // MARK: Bridged Data Types
 
 extension HyperTrack.Location {
-  var json: HyperTrack.JSON {
-    .object(["latitude": .number(latitude), "longitude": .number(longitude)])
-  }
+    var json: HyperTrack.JSON {
+        .object(["latitude": .number(latitude), "longitude": .number(longitude)])
+    }
 }
 
 extension HyperTrack.LocationError {
-  var json: HyperTrack.JSON {
-    switch self {
-    case .notRunning, .starting:
-      return .string(String(describing: self))
-    case .errors(let errors):
-      return .array(errors.map(\.json))
-    @unknown default:
-      fatalError("unhandled location error")
+    var json: HyperTrack.JSON {
+        switch self {
+        case .notRunning, .starting:
+            return .string(String(describing: self))
+        case .errors(let errors):
+            return .array(errors.map(\.json))
+        @unknown default:
+            fatalError("unhandled location error")
+        }
     }
-  }
 }
 
 extension HyperTrack.HyperTrackError {
-  var json: HyperTrack.JSON {
-    .string("HyperTrackError: \(string)")
-  }
+    var json: HyperTrack.JSON {
+        .string(string)
+    }
+}
+
+extension HyperTrack.JSON.Object {
+    var json: HyperTrack.JSON { .object(self) }
 }
--- ios/HyperTrackSdk.m
+++ ios/HyperTrackSdk.m
@@ -6,45 +6,55 @@
 
 @interface RCT_EXTERN_MODULE(HyperTrackSdk,RCTEventEmitter)
 
-RCT_EXTERN_METHOD(increment: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
-
-RCT_EXTERN_METHOD(decrement: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
-
 RCT_EXTERN_METHOD(getDeviceID: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(setName:(NSString *)name
+                  resolve: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getName: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getMetadata: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(setMetadata:(NSDictionary<NSString*, id>*)metadata
+                  resolve: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getLocation: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(isAvailable: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(setAvailability:(BOOL *)availability
+                  resolve: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(isTracking: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getErrors: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
 
 RCT_EXTERN_METHOD(startTracking: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
 
 RCT_EXTERN_METHOD(stopTracking: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
 
-RCT_EXTERN_METHOD(getLocation: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(addGeotag:(NSDictionary<NSString*, id>*)metadata
+                  resolve: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 
-RCT_EXTERN_METHOD(isTracking: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
-
-RCT_EXTERN_METHOD(availability: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(sync: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
 
 RCT_EXTERN_METHOD(subscribeToLocation: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
 
-RCT_EXTERN_METHOD(subscribeToIsTracking: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(subscribeToAvailability: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(subscribeToTracking: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
 
 RCT_EXTERN_METHOD(subscribeToErrors: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
-
-RCT_EXTERN_METHOD(setDeviceName:(NSString *)deviceName
-                  resolve: (RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
 
 RCT_EXTERN_METHOD(cancelSubscription:(NSString *)subscriptionName
                   resolve: (RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 
-RCT_REMAP_METHOD(multiply,
-                 multiplyWithA:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-{
-  NSNumber *result = @([a floatValue] * [b floatValue]);
-
-  resolve(result);
-}
+RCT_EXTERN_METHOD(cancelAllSubscriptions: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
 
 @end
 
