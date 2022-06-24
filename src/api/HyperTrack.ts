56270139d7cdf3a1bfee38a91d60a80befeeeb31
Egor Timoshenko <poter.t1mox@gmail.com>
2022-06-24 14:04:03 +0300

    added methods from HyperTrack Generation SDK to SDK wrapper

--- src/api/HyperTrack.ts
+++ src/api/HyperTrack.ts
@@ -1,5 +1,15 @@
-import { NativeModules, Platform, NativeEventEmitter } from 'react-native';
-import type { Location, LocationError, TrackingState } from '../types';
+import {
+  NativeModules,
+  Platform,
+  NativeEventEmitter,
+  EmitterSubscription,
+} from 'react-native';
+import type {
+  HyperTrackErrors,
+  Location,
+  LocationError,
+  Metadata,
+} from '../types';
 
 const LINKING_ERROR =
   `The package 'react-native-hypertrack-sdk' doesn't seem to be linked. Make sure: \n\n` +
@@ -30,56 +40,7 @@ const EventEmitter = new NativeEventEmitter(HyperTrackSdk);
  */
 export default class HyperTrack {
   /**
-   * Listen for changes of the Tracking State.
-   *
-   * @example
-   * ```js
-   * const subscription = HyperTrack.onTrackingStateChanged(trackingState => {
-   *   if (trackingState === 'started') {
-   *     // ... ready to go
-   *   }
-   * })
-   *
-   * // later, to stop listening
-   * subscription.remove()
-   * ```
-   */
-  static onTrackingStateChanged(
-    listener: (trackingState: TrackingState) => void
-  ) {
-    return EventEmitter.addListener('onTrackingStateChanged', listener);
-  }
-
-  static onLocationChanged(
-    listener: (location: { latitude: number; longitude: number }) => void
-  ) {
-    return EventEmitter.addListener('onLocationChanged', listener);
-  }
-
-  static onErrors(listener: (errors: any) => void) {
-    return EventEmitter.addListener('onErrors', listener);
-  }
-
-  static onIncrementIncreasedChanged(
-    listener: (trackingState: number) => void
-  ) {
-    return EventEmitter.addListener('onIncrement', listener);
-  }
-
-  static onIncrementDecreasedChanged(
-    listener: (trackingState: number) => void
-  ) {
-    return EventEmitter.addListener('onDecrement', listener);
-  }
-
-  static async increase(): Promise<number> {
-    return HyperTrackSdk.increment();
-  }
-  static async decrease(): Promise<number> {
-    return HyperTrackSdk.decrement();
-  }
-
-  /**
+   * Gets the unique device ID
    *
    * @returns A string used to identify a device uniquely
    */
@@ -88,8 +49,46 @@ export default class HyperTrack {
   }
 
   /**
+   * Gets device name from the dashboard
    *
-   * @returns current latitude and longitude or location error or LocationError
+   * @returns device name
+   */
+  static async getName(): Promise<void> {
+    return HyperTrackSdk.getName();
+  }
+
+  /**
+   * Allows you to set device name to be shown in dashboard
+   *
+   * @param name device name
+   * @returns void
+   */
+  static async setName(name: string): Promise<void> {
+    return HyperTrackSdk.setName(name);
+  }
+
+  /**
+   * Gets metadata associated with device
+   *
+   * @returns metadata
+   */
+  static async getMetadata(): Promise<Metadata> {
+    return HyperTrackSdk.getMetadata();
+  }
+
+  /**
+   * Allows you to set metadata to be associated with device
+   *
+   * @param  metadata metadata to be associated with device
+   */
+  static async setMetadata(metadata: Metadata): Promise<void> {
+    return HyperTrackSdk.setMetadata(metadata);
+  }
+
+  /**
+   * Gets current latitude and longitude of device or LocationError
+   *
+   * @returns current latitude and longitude or LocationError
    */
   static async getLocation(): Promise<Location | LocationError> {
     return HyperTrackSdk.getLocation();
@@ -101,11 +100,40 @@ export default class HyperTrack {
    * @returns true when is available or false when unavailable
    */
   static async isAvailable(): Promise<boolean> {
-    return HyperTrackSdk.availability();
+    return HyperTrackSdk.isAvailable();
+  }
+
+  /**
+   * Allows you to set device availability for nearby search
+   *
+   * @param availability true when is available or false when unavailable
+   */
+  static async setAvailability(availability: Boolean): Promise<void> {
+    return HyperTrackSdk.setAvailability(availability);
+  }
+
+  /**
+   * Reflects tracking intent
+   *
+   * @returns true when device is tracking or false when device is not tracking
+   */
+  static async isTracking(): Promise<boolean> {
+    return HyperTrackSdk.isTracking();
+  }
+
+  /**
+   * A list of errors that can prevent the app from successfuly tracking.
+   *
+   * @returns Returns a list of HyperTrackError
+   */
+  static async getErrors(): Promise<Array<HyperTrackErrors>> {
+    return HyperTrackSdk.getErrors();
   }
 
   /**
    * Expresses an intent to start location tracking
+   *
+   * @returns void
    */
   static async startTracking(): Promise<void> {
     return HyperTrackSdk.startTracking();
@@ -113,60 +141,165 @@ export default class HyperTrack {
 
   /**
    * Expresses an intent to stop location tracking
+   *
+   * @returns void
    */
   static async stopTracking(): Promise<void> {
     return HyperTrackSdk.stopTracking();
   }
 
   /**
+   * Tag a location with metadata
    *
-   *
-   * @returns
+   * @param metadata Metadata to be associated with geotag
+   * @returns a location that the geotag was attached to as Result<Location, LocationError>
    */
-  static async isTracking(): Promise<boolean> {
-    return HyperTrackSdk.isTracking();
+  static async addGeotag(
+    metadata: Metadata
+  ): Promise<Location | LocationError> {
+    return HyperTrackSdk.addGeotag(metadata);
   }
 
   /**
+   * Manually synchronizes tracking and availability intents with the cloud
    *
-   *
-   * @returns
+   * @returns void
    */
-  static async subscribeToLocation(): Promise<boolean> {
-    return HyperTrackSdk.subscribeToLocation();
+  static async sync(): Promise<void> {
+    return HyperTrackSdk.sync();
   }
 
   /**
+   * Listen for location.
    *
+   * @param listener (location: { latitude: number; longitude: number }) => void
+   * @returns EmitterSubscription
+   * @example
+   * ```js
+   * const subscription = HyperTrack.subscribeToLocation(location => {
+   *   const { latitude, longitude } = location;
+   * })
    *
-   * @returns
+   * // later, to stop listening
+   * HyperTrack.clearSubscriptionToTracking(subscription)
+   * ```
    */
-  static async subscribeToIsTracking(): Promise<boolean> {
-    return HyperTrackSdk.subscribeToIsTracking();
+  static subscribeToLocation(listener: (location: Location) => void) {
+    HyperTrackSdk.subscribeToLocation();
+    return EventEmitter.addListener('onLocationChanged', listener);
   }
 
   /**
-   *
-   *
-   * @returns
+   * Clears current location observers.
+   * @param eventlistener to remove
+   * @returns void
    */
-  static async subscribeToErrors(): Promise<boolean> {
-    return HyperTrackSdk.subscribeToErrors();
+  static clearSubscriptionToLocation(eventlistener: EmitterSubscription) {
+    HyperTrackSdk.cancelSubscription('unsubscribeToLocation');
+    return eventlistener.remove();
   }
 
   /**
+   * Listen for availability.
    *
+   * @param listener (isAvailable: boolean) => void
+   * @returns EmitterSubscription
+   * @example
+   * ```js
+   * const subscription = HyperTrack.subscribeToAvailability(isAvailable => {
+   *   if (isAvailable) {
+   *     // ... ready for nearby search.
+   *   }
+   * })
    *
-   * @param  deviceName A device name to be shown in dashboard
+   * // later, to stop listening
+   * HyperTrack.clearSubscriptionToAvailability(subscription)
+   * ```
    */
-  static async setDeviceName(deviceName: string): Promise<void> {
-    return HyperTrackSdk.setDeviceName(deviceName);
+  static subscribeToAvailability(listener: (isAvailable: boolean) => void) {
+    HyperTrackSdk.subscribeToAvailability();
+    return EventEmitter.addListener('onAvailableStateChanged', listener);
   }
 
   /**
+   * Clears current availability observers.
+   * @param eventlistener to remove
+   * @returns void
+   */
+  static clearSubscriptionToAvailability(eventlistener: EmitterSubscription) {
+    HyperTrackSdk.cancelSubscription('unsubscribeToAvailability');
+    eventlistener.remove();
+  }
+
+  /**
+   * Listen for tracking changes.
    *
+   * @param listener (isTracking: boolean) => void
+   * @returns EmitterSubscription
+   * @example
+   * ```js
+   * const subscription = HyperTrack.subscribeToTracking(isTracking => {
+   *   if (isTracking) {
+   *     // ... ready to go
+   *   }
+   * })
    *
-   * @param  subscriptionName A device name to be shown in dashboard
+   * // later, to stop listening
+   * HyperTrack.clearSubscriptionToTracking(subscription)
+   * ```
+   */
+  static subscribeToTracking(listener: (isTracking: boolean) => void) {
+    HyperTrackSdk.subscribeToTracking();
+    return EventEmitter.addListener('onTrackingStateChanged', listener);
+  }
+
+  /**
+   * Clears current tracking observers.
+   * @param eventlistener to remove
+   * @returns void
+   */
+  static clearSubscriptionToTracking(eventlistener: EmitterSubscription) {
+    HyperTrackSdk.cancelSubscription('unsubscribeToTracking');
+    eventlistener.remove();
+  }
+
+  /**
+   * Listen for errors.
+   *
+   * @param listener (errors: [string]) => void
+   * @returns EmitterSubscription
+   * @example
+   * ```js
+   * const subscription = HyperTrack.subscribeToErrors(errors => {
+   *   errors.forEach(error => {
+   *     // ... error
+   *   })
+   * })
+   *
+   * // later, to stop listening
+   * HyperTrack.clearSubscriptionToErrors(subscription)
+   * ```
+   */
+  static subscribeToErrors(listener: (errors: HyperTrackErrors[]) => void) {
+    HyperTrackSdk.subscribeToErrors();
+    return EventEmitter.addListener('onErrors', listener);
+  }
+
+  /**
+   * Clears current errors observers.
+   * @param eventlistener to remove
+   * @returns void
+   */
+  static clearSubscriptionToErrors(eventlistener: EmitterSubscription) {
+    HyperTrackSdk.cancelSubscription('unsubscribeToErrors');
+    eventlistener.remove();
+  }
+
+  /**
+   * Allows you to cancel specific observer
+   *
+   * @param subscriptionName A subscription name
+   * @returns void
    */
   static async cancelSubscription(subscriptionName: string): Promise<void> {
     return HyperTrackSdk.cancelSubscription(subscriptionName);
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,46 +1,36 @@
-/** The possible tracking states. */
-export enum TrackingState {
-  /** HyperTrack stopped tracking. */
-  STOPPED = 'stopped',
-
-  /** HyperTrack started tracking. */
-  STARTED = 'started',
-}
-
 export interface Location {
   latitude: number;
   longitude: number;
 }
 
-export enum LocationError {
-  // GPS satellites are not in view.
-  GPS_SIGNAL_LOST = 'gpsSignalLost',
-  // The user enabled mock location app while mocking locations is prohibited.
-  LOCATION_MOCKED = 'locationMocked',
-  // The user has not chosen whether the app can use location services. SDK can’t ask for permissions in background.
-  LOCATION_PERMISSIONS_CANT_BE_ASKED_IN_BACKGROUND = 'locationPermissionsCantBeAskedInBackground',
-  // The user denied location permissions.
-  LOCATION_PERMISSION_DENIED = 'locationPermissionsDenied',
-  // Can’t start tracking in background with When In Use location permissions. SDK will automatically start tracking when app will return to foreground.
-  LOCATION_PERMISSIONS_INSUFFICIENT_FOR_BACKGROUND = 'locationPermissionsInsufficientForBackground',
-  // The user has not chosen whether the app can use location services. SDK automatically asked for permissions.
-  LOCATION_PERMISSIONS_NOT_DETERMINED = 'locationPermissionsNotDetermined',
-  // The user didn’t grant precise location permissions or downgraded permissions to imprecise.
-  LOCATION_PERMISSIONS_REDUCED_ACCURACY = 'locationPermissionsReducedAccuracy',
-  // The app is not authorized to use location services.
-  LOCATION_PERMISSIONS__RESTRICTED = 'locationPermissionsRestricted',
-  // The user disabled location services systemwide.
-  LOCATION_SERVICES_DISABLED = 'locationServicesDisabled',
-  // The user has not chosen whether the app can use motion activity services. SDK can’t ask for permissions in background.
-  MOTION_ACTIVITY_PERMISSIONS_CANT_BE_ASKED_IN_BACKGROUND = 'motionActivityPermissionsCantBeAskedInBackground',
-  // Motion activity permissions denied after SDK’s initialization. Granting them will restart the app, so in effect, they are denied during this app’s session.
-  MOTION_ACTIVITY_PERMISSIONS_DENIED = 'motionActivityPermissionsDenied',
-  // The user has not chosen whether the app can use motion activity services. SDK automatically asked for permissions.
-  MOTION_ACTIVITY_PERMISSIONS_NOT_DETERMINED = 'motionActivityPermissionsNotDetermined',
-  // The user disabled motion services systemwide.
-  MOTION_ACTIVITY_SERVICES_DISABLED = 'motionActivityServicesDisabled',
-  // The SDK is not collecting locations because it’s either not tracking or is unavailable.
-  NOT_RUNNING = 'notRunning',
-  // The SDK started collecting locations and is waiting for OS location services to respond.
-  STARTING = 'starting',
+enum LocationRunningError {
+  NOT_RUNNING = 'notRunning', // The SDK is not collecting locations because it’s either not tracking or is unavailable.
+  STARTING = 'starting', // The SDK started collecting locations and is waiting for OS location services to respond.
+}
+
+export type LocationError = LocationRunningError | Array<HyperTrackErrors>;
+
+export interface Metadata {
+  [key: string]: any;
+}
+
+export enum HyperTrackErrors {
+  GPS_SIGNAL_LOST = 'gpsSignalLost', // The user enabled mock location app while mocking locations is prohibited.
+  LOCATION_MOCKED = 'locationMocked', // The user denied location permissions.
+  LOCATION_PERMISSIONS_DENIED = 'locationPermissionsDenied', // Can’t start tracking in background with When In Use location permissions. SDK will automatically start tracking when app will return to foreground.
+  LOCATION_PERMISSIONS_INSUFFICIENT_FOR_BACKGROUND = 'locationPermissionsInsufficientForBackground', // The user has not chosen whether the app can use location services. SDK automatically asked for permissions.
+  LOCATION_PERMISSIONS_NOT_DETERMINED = 'locationPermissionsNotDetermined', // [iOS only] The user didn’t grant precise location permissions or downgraded permissions to imprecise.
+  LOCATION_PERMISSIONS_REDUCED_ACCURACY = 'locationPermissionsReducedAccuracy', // The app is in Provisional Always authorization state, which stops sending locations when app is in background.
+  LOCATION_PERMISSIONS_PROVISIONAL = 'locationPermissionsProvisional', // [iOS only] The app is not authorized to use location services.
+  LOCATION_PERMISSIONS_RESTRICTED = 'locationPermissionsRestricted', // [iOS only] The user disabled location services systemwide.
+  LOCATION_SERVICES_DISABLED = 'locationServicesDisabled', // The device doesn't have location services.
+  LOCATION_SERVICES_UNAVAILABLE = 'locationServicesUnavailable', // [Android only] Motion activity permissions denied after SDK’s initialization. Granting them will restart the app, so in effect, they are denied during this app’s session.
+  MOTION_ACTIVITY_PERMISSIONS_DENIED = 'motionActivityPermissionsDenied', // The user has not chosen whether the app can use motion activity services. SDK automatically asked for permissions.
+  MOTION_ACTIVITY_PERMISSIONS_NOT_DETERMINED = 'motionActivityPermissionsNotDetermined', // [iOS only] The user disabled motion services systemwide.
+  MOTION_ACTIVITY_SERVICES_DISABLED = 'motionActivityServicesDisabled', // [iOS only] The device doesn't have motion activity services.
+  MOTION_ACTIVITY_SERVICES_UNAVAILABLE = 'motionActivityServicesUnavailable', // [iOS only] The SDK is not collecting locations because it’s neither tracking nor available.
+  NOT_RUNNING = 'notRunning', // The SDK started collecting locations and is waiting for OS location services to respond.
+  STARTING = 'starting', // HyperTrack Publishable key is invalid.
+  INVALID_PUBLISHABLE_KEY = 'invalidPublishableKey', // The SDK was remotely blocked from running.
+  BLOCKED_FROM_RUNNING = 'blockedFromRunning',
 }
