cd002198762f4b2c6c13e5ba61e13f42af2f1e7f
Eugene Tulushev <eugene@tulushev.com>
2022-08-05 21:04:31 +0300

    Remove unnecessary files

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  HyperTrackSdkExample
-//
-
-import Foundation
--- example/react-native.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  dependencies: {
-    'hypertrack-sdk-react-native': {
-      root: path.join(__dirname, '..'),
-    },
-  },
-};
