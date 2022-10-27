075a4682293b6cce996066a5f8b5984f2c7b46fa
Egor Timoshenko <poter.t1mox@gmail.com>
2022-10-27 12:50:12 +0300

    export types

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
 import HyperTrack from './HyperTrack/HyperTrack';
 export default HyperTrack;
 export { HyperTrack };
+
+export { HyperTrackError } from './HyperTrack/data_types/HyperTrackError';
+export { LocationError } from './HyperTrack/data_types/LocationError';
+export { Location } from './HyperTrack/data_types/Location';
