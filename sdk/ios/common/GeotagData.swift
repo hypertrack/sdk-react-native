2c94a0368ba3d54ff2cc168aebf9d9c92c3f8f7a
pavel-kuznetsov-hypertrack <pavel@hypertrack.io>
2024-04-15 16:07:07 +0300

    Add Swift code

--- sdk/ios/common/GeotagData.swift
+++ sdk/ios/common/GeotagData.swift
@@ -3,9 +3,18 @@ import HyperTrack
 struct GeotagData {
     let data: [String: Any]
     let expectedLocation: HyperTrack.Location?
+    let orderHandle: String?
+    let orderStatus: HyperTrack.OrderStatus?
 
-    init(data: [String: Any], expectedLocation: HyperTrack.Location?) {
+    init(
+        data: [String: Any],
+        expectedLocation: HyperTrack.Location?,
+        orderHandle: String?,
+        orderStatus: HyperTrack.OrderStatus?
+    ) {
         self.data = data
         self.expectedLocation = expectedLocation
+        self.orderHandle = orderHandle
+        self.orderStatus = orderStatus
     }
 }
