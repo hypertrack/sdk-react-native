896323c06a7c3121a19d2241b6b762f2a5275165
ferologics <f.hetes@gmail.com>
2024-08-21 15:38:36 +0200

    bump TS to 5.5.0

--- sdk/src/index.tsx
+++ sdk/src/index.tsx
@@ -3,12 +3,15 @@ export default HyperTrack;
 export { HyperTrack };
 
 export { HyperTrackError } from './HyperTrack/data_types/HyperTrackError';
-export {
+export type {
   NotRunning,
   Starting,
 } from './HyperTrack/data_types/internal/LocationErrorInternal';
-export { LocationError, Errors } from './HyperTrack/data_types/LocationError';
-export { LocationWithDeviation } from './HyperTrack/data_types/LocationWithDeviation';
-export { Location } from './HyperTrack/data_types/Location';
-export { Result, Success, Failure } from './HyperTrack/data_types/Result';
-export { OrderStatus } from './HyperTrack/data_types/OrderStatus';
+export type {
+  LocationError,
+  Errors,
+} from './HyperTrack/data_types/LocationError';
+export type { LocationWithDeviation } from './HyperTrack/data_types/LocationWithDeviation';
+export type { Location } from './HyperTrack/data_types/Location';
+export type { Result, Success, Failure } from './HyperTrack/data_types/Result';
+export type { OrderStatus } from './HyperTrack/data_types/OrderStatus';
